8ff6b5ac568acad43dca2be6125e362d8a909721
Dylan Thomson <djthomson88@gmail.com>
2018-12-26 22:33:27 -0500

    Add JS, start adding canvas

---
+++ assets/javascript/script.js
@@ -0,0 +1,12 @@
+// window.onload = () => {
+//     let canvas = document.getElementById("home-canvas");
+//     if (canvas.getContext) {
+//         var ctx = canvas.getContext('2d');
+    
+//         ctx.fillStyle = '#086CA2';
+//         ctx.fillRect(50, 50, 1, 1);
+    
+//         ctx.fillStyle = '#FF8B00';
+//         ctx.fillRect(30, 30, 1, 1);
+//      }
+// }
