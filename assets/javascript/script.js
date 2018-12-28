fdf63cadd3dc0b57e0db7519eae1ba9ff80c9835
Dylan Thomson <djthomson88@gmail.com>
2018-12-28 14:39:47 -0500

    Highlight current nav-item when scrolled to that part of the page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,27 @@
 $(document).ready(()=> {
 
+    $("#btn-view-work").on("click", function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, 500);
+    });
+
     $(".nav-link").on("click", function(event) {
         event.preventDefault();
         $('html, body').animate({
             scrollTop: $($.attr(this, 'href')).offset().top
         }, 500);
     });
-
-    $("#btn-view-work").on("click", function(event) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: $("#about").offset().top
-        }, 500);
+    
+    $(window).on("scroll", function() {
+        $("section").each(function() {
+            if($(window).scrollTop() >= $(this).offset().top) {
+                const id = $(this).attr("id");
+                $(".nav-item").removeClass('active');
+                $("a[href=\"#" + id + "\"]").parent().addClass("active");
+            }
+        });
     });
 
 });
