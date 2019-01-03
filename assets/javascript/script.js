cc616eea48271530a0b36c373fd197a1ce257183
Dylan Thomson <djthomson88@gmail.com>
2019-01-03 00:07:42 -0500

    Add project modals

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -15,7 +15,8 @@ $(document).ready(()=> {
     });
 
     $(".btn-learn-more").on("click", function(event) {
-        $("#modal-learn-more").modal("show");
+        const project = projects[$(this).data("project")];
+        displayProjectModal(project);
     });
     
     $(window).on("scroll", function() {
@@ -40,4 +41,59 @@ function setNavLinkActive() {
             $("a[href=\"#" + id + "\"]").parent().addClass("active");
         }
     });
+}
+
+function displayProjectModal(project) {
+    $("#learn-more-title").text(project.title);
+    $("#learn-more-subtitle").text(project.subtitle);
+    $("#learn-more-description").text(project.description);
+    $("#link-repo").attr("href", project.repoLink);
+    $("#link-live").attr("href", project.liveLink);
+
+    $("#modal-learn-more").modal("show");
+}
+
+const projects = {
+    tinfur: {
+        title: "TinFur",
+        subtitle: "Tinder for pets",
+        description: "Create an account then search for local pets in your area, Tinder style. The website uses the Petfinder API to find local pets and users can swipe left/right to skip or save as a favorite. The users can click a favorite to get more information, including how to contact the shelter. This was a group project created using jQuery, jQuery Mobile, and Google Firebase.",
+        repoLink: "https://github.com/Dylan-Thomson/tinfur",
+        liveLink: "https://dylan-thomson.github.io/tinfur/"
+    },
+    rps: {
+        title: "Multiplayer Rock Paper Scissors",
+        subtitle: "A multiplayer rock paper scissors game built with Firebase",
+        description: "Uses Google Firebase to store player and game data. Players on different devices can go head to head in a game of rock paper scissors and talk smack in chat.",
+        repoLink: "https://github.com/Dylan-Thomson/RPS-Multiplayer",
+        liveLink: "https://dylan-thomson.github.io/RPS-Multiplayer/"
+    },
+    giftastic: {
+        title: "GifTastic",
+        subtitle: "Search and favorite GIFs from GIPHY",
+        description: "Search for GIFs on GIPHY by category. The user can share these on Facebook/Twitter, and save them as favorites using local storage.",
+        repoLink: "https://github.com/Dylan-Thomson/GifTastic",
+        liveLink: "https://dylan-thomson.github.io/GifTastic/"
+    },
+    rpg: {
+        title: "Overwatch RPG",
+        subtitle: "A simple game built with jQuery",
+        description: "Choose a fighter and an oppenent. Your fighter gets stronger with each attack made, but beware of enemy counterattacks! Player must grind enemies with lower counterattack power in order to defeat them all without dying.",
+        repoLink: "https://github.com/Dylan-Thomson/unit-4-game",
+        liveLink: "https://dylan-thomson.github.io/unit-4-game/"
+    },
+    trivia: {
+        title: "Dunder Mifflin Trivia",
+        subtitle: "\"The Office\" themed trivia game",
+        description: "Answer a series of timed questions about Dunder Mifflin employees. Uses JavaScript timer functions setInterval and setTimeout",
+        repoLink: "https://github.com/Dylan-Thomson/TriviaGame",
+        liveLink: "https://dylan-thomson.github.io/TriviaGame/"
+    },
+    wordguess: {
+        title: "Word Guessing Game",
+        subtitle: "Hangman with web development acronyms",
+        description: "Try to guess what common web development acronyms mean by typing letters on the keyboard. Find every letter before running out of guesses to win! Built using vanilla JavaScript.",
+        repoLink: "https://github.com/Dylan-Thomson/Word-Guess-Game",
+        liveLink: "https://dylan-thomson.github.io/Word-Guess-Game/"
+    }
 }
